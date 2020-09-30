574bede48f1ad197b8d334bf406870b6f1647df3
@zhihua1986 <lhlaishui@163.com>
2020-09-30 11:09:47 +0800

    学习练习

---
+++ MyLib/MyLib/MyLib/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class Class1
+    {
+        public int add( int a,int b){
+            return a + b;
+            }
+    }
+}
