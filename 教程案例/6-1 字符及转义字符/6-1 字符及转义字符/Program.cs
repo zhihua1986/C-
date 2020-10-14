85d6b6e1f305b34bb522d7e865e086c14075a59d
@zhihua1986 <lhlaishui@163.com>
2020-10-14 19:40:15 +0800

    更新方法的 8.3重载实例

---
+++ 教程案例/6-1 字符及转义字符/6-1 字符及转义字符/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_1_字符及转义字符
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char mychar1 = '0';
+            char mychar2 = 'a';
+            char mychar = '男'
+            Console.WriteLine(mychar + "" + mychar1"" + mychar2);
+        }
+    }
+}
---
+++ 教程案例/8-8 方法的重载/8-8 方法的重载/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_8_方法的重载
+{
+    class Program
+    {
+        //重载写在同一个类中
+        //求元
+        static void WriteAea(int radius)
+        {
+            double b = System.Math.PI * radius * radius;
+            Console.WriteLine("你求的面积是：" + b);
+
+        }
+        //矩形
+        static void WriteAea(int width, int length)
+        {
+            int a = width * length;
+            Console.WriteLine("你求的矩形的面积是；" + a);
+
+        }
+        //三角形
+        static void WriteAea(int q, int w, int e）
+        {
+            int p = (q + w + e) / 2;
+            double area = System.Math.Sqrt(p * (p - q) * (p - w) * (p - e);
+            Console.WriteLine("你求的矩形的面积是；" + area);
+
+        }
+        static void Main(string[] args)
+        {
+            //方法调用
+            WriteAea（3,4,,5);
+            WriteAea（25,24);
+            WriteAea（10,5);
+            Console.ReadKey();
+
+
+        }
+    }
+}
